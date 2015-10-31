15cbe731ae7fabd6921e81dd3d3a2d4c5b640254
Wei Zhao <cp3hnu@gmail.com>
2015-10-31 18:18:53 +0800

    Initial Commit

---
+++ VPN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VPN
+//
+//  Created by CP3 on 15/10/31.
+//  Copyright © 2015年 CP3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
